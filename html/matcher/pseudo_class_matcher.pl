7d91a0f596e4e021056c3c414af1a7e452e26e46
gabelopes <gabriellopes.nunes@hotmail.com>
2022-09-15 10:50:54 -0300

    Implements and fixes nth-child and nth-last-child classes

--- html/matcher/pseudo_class_matcher.pl
+++ html/matcher/pseudo_class_matcher.pl
@@ -7,6 +7,14 @@ match(_, 1, _, _, first_child).
 match(_, Position, _, _, nth_child(nth(0, Position))).
 match(_, Position, _, _, nth_child(nth(A, B))) :-
   N is (Position - B) / A,
-  integer(N).
+  is_natural(N).
+match(Siblings, Position, _, _, nth_last_child(Expression)) :-
+  length(Siblings, Length),
+  SwappedPosition is Length - Position + 1,
+  match(_, SwappedPosition, _, _, nth_child(Expression)).
 match(Siblings, Position, _, _, last_child) :-
   length(Siblings, Position).
+
+is_natural(Number) :-
+  Number >= 0,
+  integer(Number).
